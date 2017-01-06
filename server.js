a05ed0c795418683f0c9326f770c9202003536c3
Alexander Aguina <aaguina@gmail.com>
2017-01-06 17:42:03 -0200

    Added ID search

--- server.js
+++ server.js
@@ -2,8 +2,60 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+var todos = [{
+  id: 1,
+  description: 'Meet mom for lunch',
+  completed: false,
+}, {
+  id: 2,
+  description: 'Go to market',
+  completed: false
+}, {
+  id: 3,
+  description: 'Take doggie to walk.',
+  completed: true
+}];
+
 app.get('/', function (req, res) {
+  // console.log(req);
   res.send('Todo API Root');
+});
+
+// GET /todos
+// GET /todos/:id
+
+app.get('/todos', function (req, res) {
+  res.json(todos);
+});
+
+app.get('/todos/:id', function (req, res) {
+  var todoId = parseInt(req.params.id, 10);
+  // console.log(todoId);
+  // var foundTodo = false;
+  var matchedTodo;
+
+  todos.forEach( function (todo) {
+    // console.log(todo.id + 'id type: ' + typeof(todo.id));
+    // if (todoId === todo.id) {
+    //   // console.log('Found Todo!');
+    //   res.json(todo);
+    //   foundTodo = true;
+    // }
+
+    // Andrew's response
+    if (todoId === todo.id) {
+      matchedTodo = todo;
+    }
+  });
+  // if (foundTodo === false) {
+  //   // console.log('Todo not found.');
+  //   res.status(404).send();
+  // }
+  if (matchedTodo) {
+    res.json(matchedTodo);
+  } else {
+    res.status(404).send();
+  }
 });
 
 app.listen(PORT, function () {
